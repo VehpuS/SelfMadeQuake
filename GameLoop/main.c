9393a67de1a552110574632ca4b10fec16b8daa7
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2016-02-02 01:14:16 +0200

    Created the game loop itself, and handling some basic behaviors on the window (closing, pressing a button).

--- GameLoop/main.c
+++ GameLoop/main.c
@@ -1,6 +1,23 @@
 #include "common.h"
 #include "winquake.h"
 
+bool isRunning = true;
+// Resolution
+int windowWidth = 800;
+int windowHeight = 600;
+
+// Tests
+// Test window size by spliting it into four equal rectangles
+void testWindowSize(HWND window, const int width, const int height, const DWORD color) {
+	HDC DeviceContext = GetDC(window);
+	PatBlt(DeviceContext, 0, 0, width / 2, height / 2, color);
+
+	PatBlt(DeviceContext, width / 2, height / 2, width / 2, height / 2, color);
+
+	ReleaseDC(window, DeviceContext);
+}
+
+
 // Event processing
 // An application-defined function that processes messages sent to a window.
 // The WNDPROC type defines a pointer to this callback function.
@@ -11,12 +28,61 @@ LRESULT CALLBACK MainWndProc(
 	_In_ WPARAM wParam,	// Additional info on the message
 	_In_ LPARAM lParam	// Additional info on the message
 	) {
+	LRESULT result = 0;
+
 	// catch any relevant messages here
 	switch (uMsg) {
+	case WM_ACTIVATE:
+	case WM_CREATE:
+		testWindowSize(hwnd, windowWidth, windowHeight, BLACKNESS);
+		break;
+	case WM_KEYUP:
+		// Posted to the window with the keyboard focus when a nonsystem key is released
+		testWindowSize(hwnd, windowWidth, windowHeight, DSTINVERT);
+		break;
+	case WM_DESTROY:
+		isRunning = false;
+		break;
 	default:
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		break;
 	}
+	return (result);
 }
+
+HWND generateWindowByResolution(HINSTANCE hInstance,
+								LPCTSTR className,
+								LPCTSTR windowName,
+								const int xPos,
+								const int yPos,
+								const int nWidth,
+								const int nHeight) {
+
+	// Generate window style and actual width / height
+	DWORD windowStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
+	RECT rect;
+	rect.top = rect.left = 0;
+	rect.right = nWidth;
+	rect.bottom = nHeight;
+	AdjustWindowRect(&rect, windowStyle, FALSE);
+	int mainWindowWidth = rect.right - rect.left;
+	int mainWindowHeight = rect.bottom - rect.top;
+
+	// Create the window and return it
+	return CreateWindowEx(	0,
+							className,
+							windowName,
+							windowStyle,
+							xPos,
+							yPos,
+							mainWindowWidth,
+							mainWindowHeight,
+							NULL,
+							NULL,
+							hInstance,
+							0);
+}
+
 
 /****************************
 * Entry point for windows. *
@@ -55,38 +121,37 @@ WinMain(
 		return (EXIT_FAILURE);
 	}
 
-	// Generate window style and actual width / height
-	DWORD windowStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
-	RECT rect;
-	rect.top = rect.left = 0;
-	rect.right = 800;
-	rect.bottom = 600;
-	AdjustWindowRect(&rect, windowStyle, FALSE);
-	int mainWindowWidth = rect.right - rect.left;
-	int mainWindowHeight = rect.bottom - rect.top;
-
-	// Create the window and draw it
-	HWND mainWindow = CreateWindowEx(	0,
-										"GameLoop",
-										"GameLoop Window",
-										windowStyle,
-										CW_USEDEFAULT,
-										CW_USEDEFAULT,
-										mainWindowWidth,
-										mainWindowHeight,
-										NULL,
-										NULL,
-										hInstance,
-										0);
+	HWND mainWindow = generateWindowByResolution(	hInstance,
+													wc.lpszClassName,
+													"GameLoop Window",
+													CW_USEDEFAULT,
+													CW_USEDEFAULT,
+													windowWidth,
+													windowHeight);
 
 	ShowWindow(mainWindow, SW_SHOWDEFAULT);
 
-	// Test window size
-	HDC DeviceContext = GetDC(mainWindow);
+	MSG msg;  // will store a message from the OS
+	LRESULT dispatchResult;
+	while (isRunning) {
+		// Check the OS for messages
+		if (PeekMessage(&msg,			// A pointer to an MSG structure that receives message information
+						NULL,			// if hWnd is NULL, both window messages and thread messages are processed
+						0,				// The value of the first message in the range of messages to be examined
+						0,				// The value of the last message in the range of messages to be examined
+						PM_REMOVE)		// Specifies how messages are to be handled
+						) {
+			bool messageTranslated = TranslateMessage(&msg);
+			// will call MainWndProc
+			dispatchResult = DispatchMessage(&msg);
 
-	PatBlt(DeviceContext, 0, 0, 400, 300, BLACKNESS);
-	PatBlt(DeviceContext, 400, 300, 400, 300, BLACKNESS);
-	ReleaseDC(mainWindow, DeviceContext);
+			// Update game loop
+			// updateGameLoop();
+
+			// Update graphics
+			// drawGraphics();
+		}
+	}
 
 	return (EXIT_SUCCESS);
 }
